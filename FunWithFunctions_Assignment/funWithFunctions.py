daef3acd4a4f0c24b8c4bc808c3ba81d4484e47c
Holly Valenty <holly914@gmail.com>
2018-02-06 11:40:59 -0600

    Corrected Assignment

--- FunWithFunctions_Assignment/funWithFunctions.py
+++ FunWithFunctions_Assignment/funWithFunctions.py
@@ -1,3 +1,4 @@
+
 def oddOrEven():
     for index in range(1,2001):
         if index%2 != 0:
@@ -5,6 +6,7 @@ def oddOrEven():
         elif index%2 == 0:
             print "Number is", index,". This is an even number."
 oddOrEven()
+
 
 def multiply(arr,num):
     for x in range(len(arr)):
@@ -14,9 +16,19 @@ a = [2,4,10,16]
 b = multiply(a,5)
 print b
 
-def layered_multiples(arr):
+
+
+def multiply(arr,num):
     for x in range(len(arr)):
-        arr[x]*= arr
+        arr[x] *= num
     return arr
+
+def layered_multiples(arr):
+    new_array = []
+    i=0
+    while i < 3:
+        new_array.append(arr[i]* "1")
+        i+=1
+    print new_array
+
 x = layered_multiples(multiply([2,4,5],3))
-print x
