7b7a6e94c9d5f55a35c4cefe7d3ac2a1025790a0
Holly Valenty <holly914@gmail.com>
2018-02-06 19:00:48 -0600

    Turning lists into dictionaries

---
+++ MakingDictionaries_Assignment/makingDict.py
@@ -0,0 +1,18 @@
+list3 = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+list4 = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+
+
+def make_dict(list1, list2):
+    new_dict = zip(list1,list2)
+    print new_dict
+    return new_dict
+
+make_dict(list3, list4)
+
+
+  #take in two lists
+  #create 1 dictionary
+
+  #first list contains keys
+  #second list contains values 
