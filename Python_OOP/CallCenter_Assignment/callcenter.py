2a34d3408e441539d7ce394663586404a0df541d
Holly Valenty <holly914@gmail.com>
2018-02-08 13:17:50 -0600

    Python OOP

--- Python_OOP/CallCenter_Assignment/callcenter.py
+++ Python_OOP/CallCenter_Assignment/callcenter.py
@@ -18,37 +18,40 @@ class Call(object):
 
 
 class CallCenter(object):
-    def __init__(self, name, phone):
-        self.calls = 1   
-        self.queueSize = 0
-        self.name = name
-        self.phone = phone
-        self.list = [] 
-        self.add()
-        self.info()   
+    def __init__(self, list=[], calls=0):
+        self.calls = calls   
+        # self.name = name
+        # self.phone = phone
+        self.list = list 
+        # self.add()
+        # self.info()   
     
-    def add(self):
+    def add(self, newCall):
         while self.calls < 4:
-        # for calls in queueSize:
             self.calls += 1
-            self.queueSize += 1
+    
         self.list.append(self.calls)
         return self
 
-    def remove(self):
+    def remove(self,done):
         self.list.pop(0)
+        self.calls -= 1
+        print "Call answered. Caller queue at:", self.calls 
         return self
 
     def info(self):
         print "Caller in Queue:", self.name
         print "Caller in Queue Phone #", self.phone
-        print "Queue Size:", self.queueSize
         print "Call list:",self.list
         print "Total Calls:",self.calls
         return self
     
-Call1 = Call(1, "Betty Boop", 1234567890, "9:01 AM", "Needs to chat")
-CallCenter1 = CallCenter("Holly",55536725555)
-CallCenter2 = CallCenter("Lesley",5555555235)
-CallCenter3 = CallCenter("Carly",5572755555)
-CallCenter4 = CallCenter("Cecily",5522255555)
+Call1 = Call(1, "Holly Valenty", 1234567890, "9:01 AM", "Needs to chat")
+Call2 = Call(2, "Lesley Valenty", 1234567890, "9:09 AM", "Has a complaint")
+Call3 = Call(3, "Carly Valenty", 1234567890, "3:15 PM", "Wants a refund")
+Call4 = Call(4, "Cecily Valenty", 1234567890, "5:09 PM", "Has a question")
+
+CallCenter1 = CallCenter()
+
+
+CallCenter1.add(Call1).add(Call2).add(Call3).add(Call4).remove(Call1)
